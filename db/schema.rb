18dabec84f67fa1c71872b95ebb2ee3d817f6a09
Miggy Pinaroc <migzpinaroc@gmail.com>
2019-02-23 22:39:16 +0800

    updated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_131145) do
+ActiveRecord::Schema.define(version: 2019_02_23_141718) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fields", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mentees", force: :cascade do |t|
     t.string "email", default: "", null: false
