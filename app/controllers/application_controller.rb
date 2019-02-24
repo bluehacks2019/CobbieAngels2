00dfb0353364560a822b10a81fa47015fd4693ae
Miggy Pinaroc <migzpinaroc@gmail.com>
2019-02-24 11:31:37 +0800

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,12 @@ class ApplicationController < ActionController::Base
     # before_action :authenticate_mentee!, except: [:index]
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
-    rescue_from NoMethodError, with: :unrestricted
+    # rescue_from NoMethodError, with: :unrestricted
 
-    def unrestricted
-        flash[:notice] = 'Error! Forced to logout user'
-        redirect_to root_path
-    end
-
-
+    # def unrestricted
+    #     flash[:notice] = 'Error! Forced to logout user'
+    #     redirect_to root_path
+    # end
 
     protected
     def configure_permitted_parameters
