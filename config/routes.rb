23791f56265c04e31f7e834f39daa38bb8b35e3d
Paolo Sanchez <paolo.sanchez@obf.ateneo.edu>
2019-02-24 09:31:54 +0800

    fixed user landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :mentees
-  devise_for :mentors
+  devise_for :mentees, controllers: { registrations: "mentees/registrations" }
+  devise_for :mentors, controllers: { registrations: "mentors/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get:'/mentee', to:'home#mentee-homepage'
+  get:'/mentor', to:'home#mentor-homepage'
   resources :mentees, :mentors
   root to: 'home#index'
 end
